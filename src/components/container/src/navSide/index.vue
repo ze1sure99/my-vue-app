b8e09c8e72d9b3992f622b8416ff3a72d7fd6c4a
ze1sure99 <1306445936@qq.com>
2022-01-19 00:38:01 +0800

    vue3组件中传值 该2-5了

---
+++ src/components/container/src/navSide/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <el-menu
+        :collapse="collapse"
+        default-active="2"
+        class="el-menu-vertical-demo"
+    >
+      <el-menu-item index="1">
+        <el-icon-menu></el-icon-menu>
+        <span>导航1</span>
+      </el-menu-item>
+      <el-menu-item index="2">
+        <el-icon-menu></el-icon-menu>
+        <span>导航2</span>
+      </el-menu-item>
+      <el-menu-item index="3">
+        <el-icon-menu></el-icon-menu>
+        <span>导航3</span>
+      </el-menu-item>
+    </el-menu>
+</template>
+
+<script lang="ts" setup>
+let props = defineProps<{
+  collapse: boolean
+}>()
+</script>
+
+<style scoped>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
+}
+</style>
